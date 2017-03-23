0d19bf45caa7ae433bccbf70b2a08ab3279e1ffe
meijun <jun.mei@outlook.com>
2017-03-23 22:00:38 +0800

    for SI211 course

--- mail.py
+++ mail.py
@@ -6,19 +6,19 @@ import time
 
 # Config your send email
 DEBUG = True
-mail_user = "sender@example.edu.cn"
-mail_pass = "YourPassword"
+mail_user = "name@shanghaitech.edu.cn"
+mail_pass = "password"
 smtp_server = 'smtp.shanghaitech.edu.cn'
 smtp_port = 25
 
 # Config your mail template
-title_template = '[{}] Your CS101 Final Exam Score'
+title_template = '[{}] Your SI221 Homework 1 Score'
 content_template = '''
 <table style="border-radius: 3px;border: 1px solid #d8d8d8;border-bottom-width: 2px;border-top-width: 0;width: 100%;">
 	<tr style="border:0px;padding:0px;margin:0px;">
 		<td style="padding:20px;text-align:center; background:#1C9EFF;color:white;padding:20px;">
 			<h2 style="border:0px;padding:0px;margin:0px;">
-				Your CS101 Final Exam Score
+				Your SI221 Homework 1 Score
 			</h2>
 			<span>
 				{}({})
@@ -28,13 +28,19 @@ content_template = '''
 	<tr style="border:0px;padding:0px;margin:0px;">
 		<td style="padding:20px;">
 			<p>
-				Here is your final exam score:
+				Here is your homework 1 score:
 			</p>
 			<p>
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>{} / 100</strong>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>{}/100</strong>
 			</p>
 			<p>
-				Happy new year!
+				Score of every problem:
+			</p>
+			<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{}
+			</p>
+			<p>
+				Have a nice day!
 			</p>
 		</td>
 	</tr>
@@ -42,15 +48,15 @@ content_template = '''
 		<td style="padding:20px;text-align:center;">
 			<hr/>
 			<p>
-				Data Structures <small>(CS101)</small>, ShanghaiTech University
+				Graph Theory <small>(SI221)</small>, ShanghaiTech University
 			</p>
 		</td>
 	</tr>
 </table>
 '''
 
-def send(mail, name, id, score):
-	msg = MIMEText(content_template.format(name, id, score), 'html')
+def send(mail, name, id, score, problem_score):
+	msg = MIMEText(content_template.format(name, id, score, [float(i) for i in problem_score]), 'html')
 	msg['From'] = mail_user
 	msg['Subject'] = title_template.format(name)
 	if DEBUG:
@@ -64,7 +70,7 @@ print('Parsing data file ... ', end = '', flush = True)
 try:
 	file = open('grade.dat')
 	lines = file.readlines()
-	data = [i.split() for i in lines]
+	data = [i.split(',') for i in lines]
 	file.close()
 	print('Success! get {} data'.format(len(data)), flush = True)
 except:
@@ -85,19 +91,20 @@ except:
 # parse data file
 print('Login sender email {} ... '.format(mail_user), end = '', flush = True)
 try:
-	server.starttls()
+	# server.starttls()
+	server.ehlo()
 	server.login(mail_user, mail_pass)
 	print('Success!', flush = True)
-except:
+except Exception as e:
 	server.quit()
-	print('Failed!', flush = True)
+	print('Failed! ', e, flush = True)
 	exit(1)
 
 for i in range(len(data)):
 	d = data[i]
 	print('Sending: {} ... '.format(d[2]), end = '', flush = True)
 	try:
-		send(d[0], d[2], d[1], d[3])
+		send(d[0], d[2], d[1], d[-1], d[3:-1])
 		print('Success!', flush = True)
 	except:
 		server.quit()
@@ -110,7 +117,8 @@ for i in range(len(data)):
 		print('Done.', flush = True)
 		server.connect(smtp_server, smtp_port)
 		# we are using TLS mathod
-		server.starttls()
+		server.ehlo()
+		# server.starttls()
 		server.login(mail_user,mail_pass)
 
 server.quit()
